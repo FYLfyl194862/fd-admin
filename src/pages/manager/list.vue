e821942f7caa3895bd5f344a91990cd7683470bd
“FYLfyl194862” <398625768@qq.com>
2023-09-23 21:19:31 +0800

    状态改变

--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -31,10 +31,13 @@
     </div>
     <!-- 表格栏 -->
     <el-table :data="tableData">
-      <el-table-column label="管理员" align="center">
+      <el-table-column label="管理员" align="center" width="200">
         <template #default="{ row }">
-          <div class="flex justify-center items-center">
-            <el-avatar :size="40" :src="row.avatar" @error="errorHandler">
+          <div class="flex items-center">
+            <el-avatar :size="40" :src="row.avatar">
+              <img
+                src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
+              />
             </el-avatar>
             <div class="ml-3">
               <h6>{{ row.username }}</h6>
@@ -51,15 +54,21 @@
       <el-table-column label="状态">
         <template #default="{ row }">
           <el-switch
+            :loading="row.statusLoading"
             :modelValue="row.status"
             :active-value="1"
             :inactive-value="0"
+            :disabled="row.super == 1"
+            @change="handleStatusChange($event, row)"
           >
           </el-switch>
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center">
         <template #default="scope">
+          <small class="text-sm text-gray-500" v-if="scope.row.super == 1"
+            >暂无操作</small
+          >
           <el-button size="small" @click="handleEdit(scope.row)" text
             >修改</el-button
           >
@@ -89,8 +98,9 @@
 </template>
 
 <script setup>
-import { reactive, ref } from "vue";
-import { getManagerList } from "~/api/manager.js";
+import { reactive, ref, toDisplayString } from "vue";
+import { getManagerList, updateManagerStatus } from "~/api/manager.js";
+import { toast } from "~/composables/util.js";
 const searchForm = reactive({
   keyword: "",
 });
@@ -100,6 +110,7 @@ const resetSearchForm = () => {
   getData();
 };
 const tableData = ref([]);
+const loading = ref(false);
 const total = ref(0);
 const limit = ref(10);
 const currentPage = ref(1);
@@ -107,12 +118,31 @@ function getData(p = null) {
   if (typeof p == "number") {
     currentPage.value = p;
   }
-  getManagerList(currentPage.value, searchForm).then((res) => {
-    console.log(res);
-    tableData.value = res.list;
-    total.value = res.totalCount;
-  });
+  loading.value = true;
+  getManagerList(currentPage.value, searchForm)
+    .then((res) => {
+      //   console.log(res);
+      tableData.value = res.list.map((o) => {
+        o.statusLoading = false;
+        return o;
+      });
+      total.value = res.totalCount;
+    })
+    .finally(() => {
+      loading.value = false;
+    });
 }
 getData();
+//修改状态
+const handleStatusChange = (status, row) => {
+  row.statusLoading = true;
+  updateManagerStatus(row.id, status)
+    .then((res) => {
+      toast("修改状态成功");
+    })
+    .finally(() => {
+      row.statusLoading = false;
+    });
+};
 </script>
 <style scoped></style>
